d461447fe9c6660bbfa523295f0eecc91be2086f
Billyas <62041096+Billyas@users.noreply.github.com>
2024-05-17 01:40:12 +0800

    .

---
+++ push.bat
@@ -0,0 +1,4 @@
+set HTTP_PROXY=socks5://127.0.0.1:2080
+set HTTPS_PROXY=socks5://127.0.0.1:2080
+
+git push
--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@arcgis/core/assets/esri/themes/light/main.css' 
-import esriConfig from "@arcgis/core/config";
 
-esriConfig.locale = "zh-cn";
 createApp(App).use(router).mount('#app')
