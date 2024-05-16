2fcbf83e4e69df46d3a36f8a3424e8da89a9bc3f
Billyas <62041096+Billyas@users.noreply.github.com>
2024-05-17 01:41:47 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BaseUrl,
+  base: '/arcgis-vue3-demo/',
   plugins: [
     vue(),
   ],
